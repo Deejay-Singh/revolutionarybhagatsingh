166c16995baf9475fa9cb17d5c534dd48c4f0d85
DJ <daljeet@inventoryplaza.com>
2013-11-16 21:30:15 +0530

    Comment SQL

---
+++ app/Config/Schema/sql/Comments.sql
@@ -0,0 +1,45 @@
+-- phpMyAdmin SQL Dump
+-- version 4.0.4
+-- http://www.phpmyadmin.net
+--
+-- Host: localhost
+-- Generation Time: Nov 16, 2013 at 09:27 PM
+-- Server version: 5.5.32-0ubuntu0.12.04.1
+-- PHP Version: 5.3.10-1ubuntu3.8
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8 */;
+
+--
+-- Database: `revolutionarybhagatsingh`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `comments`
+--
+
+CREATE TABLE IF NOT EXISTS `comments` (
+  `id` int(5) NOT NULL AUTO_INCREMENT,
+  `user_id` varchar(10) NOT NULL,
+  `user_email` varchar(50) NOT NULL,
+  `user_name` varchar(50) NOT NULL,
+  `comment` text NOT NULL,
+  `object_id` varchar(10) NOT NULL,
+  `object_type` varchar(20) NOT NULL,
+  `is_active` tinyint(1) NOT NULL DEFAULT '1',
+  `modified` datetime NOT NULL,
+  `created` datetime NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1 AUTO_INCREMENT=1 ;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
