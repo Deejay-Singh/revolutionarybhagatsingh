69545c57eb76f8c935c764f933e4c258e439eb97
DJ <daljeet@inventoryplaza.com>
2013-11-18 23:00:27 +0530

    IP Address.

--- app/Controller/CommentsController.php
+++ app/Controller/CommentsController.php
@@ -10,7 +10,7 @@ class CommentsController extends AppController {
     public function comment() {
         $data = Sanitize::clean($this->request->data, array('encode' => false));
         $user_id = 0;
-        $data = array_merge($data, array( 'user_id' => $user_id, 'is_active' => 0, 'created' => date("Y-m-d H:i:s"), 'modified' => date("Y-m-d H:i:s") ));
+        $data = array_merge($data, array( 'user_id' => $user_id, 'is_active' => 0, 'created' => date("Y-m-d H:i:s"), 'modified' => date("Y-m-d H:i:s"), $from = $_SERVER['REMOTE_ADDR'] ));
 		$this->Comment->create();
         $this->Comment->save($data);
         $this->redirect( $this->referer() );
