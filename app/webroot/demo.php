640ae5f0f1bd7436e2cf1f5d88335bb104eba75a
DJ <daljeet@inventoryplaza.com>
2014-04-13 16:18:43 +0530

    Added FK affiliate.

--- app/webroot/demo.php
+++ app/webroot/demo.php
@@ -1,1 +1,1 @@
-demo
+<a href="http://www.flipkart.com/moto-g/p/itmdsmbxcrm9wy8r?pid=MOBDSGU2QFWMHGRR&affid=daljeetwo" >http://www.flipkart.com/moto-g/p/itmdsmbxcrm9wy8r?pid=MOBDSGU2QFWMHGRR&affid=daljeetwo</a>
