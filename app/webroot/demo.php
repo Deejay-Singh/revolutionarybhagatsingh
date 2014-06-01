c47f3f6216118d7961f14e7d92ab6f3aed8535d1
DJ <daljeet@inventoryplaza.com>
2014-06-01 19:21:40 +0530

    Affiliate amazon books.

--- app/webroot/demo.php
+++ app/webroot/demo.php
@@ -3,3 +3,5 @@
 <a target="blank" href="http://www.flipkart.com/motorola/motoe?affid=daljeetwo" >motoe</a>
 <a target="blank" href="http://bit.ly/1wMRoxa" >The Diary of a Young Girl [Paperback]</a>
 <a target="blank" href="http://bit.ly/1m34KwM" >Study Package for NTSE Class X [Paperback]</a>
+SAVE35PC<iframe src="http://ws-in.amazon-adsystem.com/widgets/q?&f=ifr&MarketPlace=IN&ServiceVersion=20070822&WS=1&ID=8042_ProductLink&Operation=GetProductLink&lt1=_blank&bc1=000000&IS2=1&bg1=FFFFFF&fc1=000000&lc1=0000FF&t=wwwrevoluti06-21&o=31&p=8&l=as4&m=amazon&f=ifr&ref=ss_til&asins=B00J03LU20" style="width:120px;height:240px;" scrolling="no" marginwidth="0" marginheight="0" frameborder="0"></iframe>
+<iframe src="http://ws-in.amazon-adsystem.com/widgets/q?&f=ifr&MarketPlace=IN&ServiceVersion=20070822&WS=1&ID=8042_ProductLink&Operation=GetProductLink&lt1=_blank&bc1=000000&IS2=1&bg1=FFFFFF&fc1=000000&lc1=0000FF&t=wwwrevoluti06-21&o=31&p=8&l=as4&m=amazon&f=ifr&ref=ss_til&asins=B00KLU9MFS" style="width:120px;height:240px;" scrolling="no" marginwidth="0" marginheight="0" frameborder="0"></iframe>
