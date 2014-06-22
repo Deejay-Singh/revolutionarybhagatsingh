d7d9e215fac2226289be8fd4ccc25b6529c9facd
Daljeet Singh <er.daljeetsinghmail@gmail.com>
2014-06-22 19:23:50 +0530

    Update demo.php

--- app/webroot/demo.php
+++ app/webroot/demo.php
@@ -1,5 +1,6 @@
 <a target="blank" href="http://www.flipkart.com/moto-g/p/itmdsmbxcrm9wy8r?pid=MOBDSGU2QFWMHGRR&affid=daljeetwo" >motog 8GB</a>
 <a target="blank" href="http://www.flipkart.com/moto-g/p/itmdsmbxcrm9wy8r?pid=MOBDSGU2ZMDYENQA&affid=daljeetwo" >motog 16gb</a>
+<a target="blank" href="http://www.flipkart.com/apple-iphone-4s/p/itmdv6f7mbafcxyz?pid=MOBDQD7UHBGZHRPN&affid=daljeetwo" >Iphone 4S</a>
 <a target="blank" href="http://www.flipkart.com/motorola/motoe?affid=daljeetwo" >motoe</a>
 <a target="blank" href="http://bit.ly/1wMRoxa" >The Diary of a Young Girl [Paperback]</a>
 <a target="blank" href="http://bit.ly/1m34KwM" >Study Package for NTSE Class X [Paperback]</a>
